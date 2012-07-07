4068a8eec44af4266a5b526f20422fa076335717
Suraj <medevil13@gmail.com>
2012-07-07 21:01:07 +0530

    improved functionality

--- index.php
+++ index.php
@@ -9,34 +9,40 @@ function funhash(id){
 	var key = document.getElementById('key').value;
 	var salt = document.getElementById('salt').value;
 	var hasharea = document.getElementById('hasharea');
-	hasharea.innerHTML = '<img src="loader.gif"/>';
-		try{
-			// Opera 8.0+, Firefox, Safari
-			ajaxRequest = new XMLHttpRequest();
-		} catch (e){
-			// Internet Explorer Browsers
+	if(salt != ''){
+		
+		hasharea.innerHTML = '<img src="loader.gif"/>';
 			try{
-				ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-			} catch (e) {
+				// Opera 8.0+, Firefox, Safari
+				ajaxRequest = new XMLHttpRequest();
+			} catch (e){
+				// Internet Explorer Browsers
 				try{
-					ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-				} catch (e){
-					// Something went wrong
-					alert("Your browser broke!");
-					return false;
+					ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+				} catch (e) {
+					try{
+						ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+					} catch (e){
+						// Something went wrong
+						alert("Your browser broke!");
+						return false;
+					}
 				}
 			}
+		ajaxRequest.open('GET','hash.php?id='+id+'&key='+key+'&salt='+salt,true);
+		ajaxRequest.send(null);
+		
+		ajaxRequest.onreadystatechange = function(){
+			if(ajaxRequest.readyState ==4){
+				hasharea.innerHTML = ajaxRequest.responseText+'<img src="tick.png"/>';
+			}
+			else{
+				hasharea.innerHTML = 'Cant carry the request'; 
+			}
 		}
-	ajaxRequest.open('GET','hash.php?id='+id+'&key='+key+'&salt='+salt,true);
-	ajaxRequest.send(null);
-	
-	ajaxRequest.onreadystatechange = function(){
-		if(ajaxRequest.readyState ==4){
-			hasharea.innerHTML = ajaxRequest.responseText+'<img src="tick.png"/>';
-		}
-		else{
-			hasharea.innerHTML = 'Cant carry the request'; 
-		}
+	}
+	else{
+		hasharea.innerHTML = 'Enter the key or salt';
 	}
 }
 </script>
@@ -45,8 +51,8 @@ function funhash(id){
 <body>
   <div class="container">
   	<div class="input">
-        <span><label for="key">Key</label><input type="text" id="key" placeholder="Enter the key here"/></span>
         <span><label for="salt">Salt</label><input type="text" id="salt" placeholder="Enter the salt here"/></span>
+        <span><label for="key">Key</label><input type="text" id="key" placeholder="Enter the key here"/></span>
     </div>
     <div id="hasharea"></div>
     <div id="functions">
