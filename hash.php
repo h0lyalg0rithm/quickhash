3b74cd2732b02dcfe1189c5098f4fc398afc287f
Suraj <medevil13@gmail.com>
2012-08-08 23:53:35 +0530

    Changed from hmac to hash.

--- hash.php
+++ hash.php
@@ -3,7 +3,7 @@ $id = $_GET['id'];
 $salt = $_GET['salt'];
 $key = $_GET['key'];
 $hash_alg = hash_algos();
-$hash = hash_hmac($hash_alg[$id],$key,$salt);
+$hash = hash($hash_alg[$id],$key.$salt);
 
 echo $hash;
 
